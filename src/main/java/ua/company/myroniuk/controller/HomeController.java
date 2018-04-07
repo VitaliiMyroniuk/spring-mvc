88a13865a3da80b9136cb7837e747f058621f75e
Vitalii Myroniuk <vitalii.myroniuk@gmail.com>
2018-04-08 01:28:59 +0300

    Add JSON support. Use RequestBody and ResponseBody annotations.

--- src/main/java/ua/company/myroniuk/controller/HomeController.java
+++ src/main/java/ua/company/myroniuk/controller/HomeController.java
@@ -1,11 +1,17 @@
 package ua.company.myroniuk.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ua.company.myroniuk.entity.User;
 import javax.validation.Valid;
 
@@ -33,6 +39,24 @@ public class HomeController {
         }
         model.addAttribute("user", user);
         return "home";
+    }
+
+    @RequestMapping(path="/json/get-user/{login}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public User getJsonUser(@PathVariable(name = "login") String login,
+                            @RequestParam(name = "name") String name) {
+        User user = new User();
+        user.setLogin(login);
+        user.setName(name);
+        return user;
+    }
+
+    @RequestMapping(path="/json/post-user", method = RequestMethod.POST, consumes = "application/json")
+    public ResponseEntity<String> setJsonUser(@RequestBody User user) {
+        if (user.getLogin() == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
